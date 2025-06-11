f63344623a4b4613cfdccb3c3da38233e1ccc187
peter-ic-cooke <122986210+peter-ic-cooke@users.noreply.github.com>
2025-06-11 10:56:48 +0100

    Update run_coex.sh

--- Tutorial2/run_coex.sh
+++ Tutorial2/run_coex.sh
@@ -1,6 +1,22 @@
-timestep=1 # timestep in fs
-temp=1000 # temperature in K
-press=0 # pressure in GPa
-num_steps=200001 # number of steps
+#!/bin/sh
 
-ramble -ompnp 8 -t -te 50 -ts $timestep -dr 0 -tt $temp -p $press -m $num_steps LiH-coex > ramble.out 2> ramble.err
+export num_jobs=1
+export cores_per_job=20
+
+export total_cores=$((${num_jobs} * ${cores_per_job}))
+
+#SBATCH --job-name=ramble_LiH
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=20
+
+module load gnu12
+module load openmpi4/4.1.6
+export LD_LIBRARY_PATH=/usr/lib64/:$LD_LIBRARY_PATH
+export LIBRARY_PATH=/usr/lib64/:$LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/SharedExe/openblas:/home/SharedExe/libs/$LD_LIBRARY_PATH
+export LIBRARY_PATH=/home/SharedExe/openblas:/home/SharedExe/libs/$LD_LIBRARY_PATH
+
+
+ramble -ompnp ${cores_per_job} -t -te 10 -ts 0.1 -dr 0 -tt 500 -p 0 -m 10000 LiH-coex > ramble.out 2> ramble.err
